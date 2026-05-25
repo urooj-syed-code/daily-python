f99569da7e242ecdc7a0d858e5f0d4e3d3bbc1dd
urooj-syed-code <uroojsyeduni@gmail.com>
2026-05-25 17:55:03 -0500

    making a simple calculator

---
+++ Day1.py
@@ -0,0 +1,7 @@
+#calculator
+a= int(input("Enter first number:"))
+b= int(input("Enter the second number:"))
+
+print("The sum is:", a+b)
+print("the difference is:", a-b)
+print("product is:", a*b)
